86f7211a69c82fbe26cc744c3bc05f8db75b11cd
gzapantis <gzapantis@cepal.gr>
2018-09-24 11:15:41 +0300

    Initial Laravel Commit

---
+++ laravelcmsnew/app/Post.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+    protected $table = 'post'; // define that the database table is different than the default one
+}
