a7b273b8d5751779209fdf50ac0816b9f5c57ef5
Grigoris Zapantis <gzapas@gmail.com>
2025-07-28 10:01:13 +0300

    inventory management

---
+++ BlazorInventoryManagementSystemNet8/IMS/IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -0,0 +1,26 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases.Inventories
+{
+    public class ViewInventoriesByNameUseCase
+    {
+        #region Fields
+        private readonly IInventoryRepository _inventoryRepository;
+        #endregion
+
+        #region Constructor
+        public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+        {
+            _inventoryRepository = inventoryRepository;
+        }
+        #endregion
+
+        #region Methods
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "") 
+        {
+            return await _inventoryRepository.GetInventoriesByNameAsync(name);
+        }
+        #endregion
+    }
+}
